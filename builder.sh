3a2916301c1a4be81993e4b55fa3e9a871fe63af
Philip Callender <philip.callender@tooltwist.com>
2015-10-11 06:53:46 -0400

    wip

--- builder.sh
+++ builder.sh
@@ -247,12 +247,11 @@ function doBuild {
 #	rm -rf deploy/docker/.tooltwist/webdesign-projects/${app}
 	
 	(
-		echo1 '$ cd ${HOME}/apps/${app}/deploy/docker'
-		echo1 "${RED}cd" `pwd` "${BLUE}"
-		cd deploy/docker
-		trap true SIGINT
+		echo1 "$ cd ${HOME}/apps/${app}/deploy/docker"
+		         cd ${HOME}/apps/${app}/deploy/docker
 
-		echo1 "${RED}$ tooltwist -n docker${BLUE}"
+		trap true SIGINT
+		echo1 "${RED}$ tooltwist -n docker${AQUA}"
 		               tooltwist docker < /dev/null
 		echo1 "${BLUE}\n"
 		trap - SIGINT
@@ -449,16 +448,6 @@ cat << END
 	  6. Show image on this machine
 	  7. Show image on Docker Hub
 	  8. Push image to Docker hub
-
-	Run:
-	  10. Map application to a swarm
-	  11. Edit docker-compose.yml
-	  12. Start application
-	  13. Stop application
-	  14. Restart application
-	  15. Remove application
-
-	  9. View swarm
 
 	  s. Shell
 	  f. Finish with this App
@@ -799,20 +788,26 @@ function maintainSingleSwarm {
 	Menu:
 	  1. Swarm Containers
 	  2. Swarm Info
-  
-	  3. Start proxy
-	  4. Stop proxy
-	  5. Restart proxy
-	  6. View proxy config
-	  7. View proxy log
 	  
-	  8. Map application to this swarm
-	  9. Edit docker-compose.yml
+	  3. Map application to this swarm
 
-	  10. Start application
-	  11. Stop application
-	  12. Restart application
-	  13. Remove application
+	  Application
+	  11. Tail log
+	  12. Start
+	  13. Stop
+	  14. Restart
+	  15. Stop and remove container
+	  16. Edit docker-compose.yml
+	  17. Shell in config directory
+  
+	  Proxy
+	  91. Tail log
+	  92. Start
+	  93. Stop
+	  94. Restart
+	  95. Remove container
+	  96. View proxy config
+	  97. Login to proxy container
   
 	  s. Shell
 	  f. Finish with this menu
@@ -830,46 +825,58 @@ END
 			2)
 				swarmInfo ${swarm}
 				;;
-			3)
-				proxyOp ${swarm} Start
-				;;
-			4)
-				proxyOp ${swarm} Stop
-				;;
-			5)
-				proxyOp ${swarm} Restart
-				;;
-
-			# Remove proxy container?
-
-			6)
-				showProxyConfig ${swarm}
-				;;
-			7)
-				followProxyLog ${swarm}
-				;;
 
 			# App definitions
-			8)
+			3)
 				mapAppToSwarm ${swarm}
 				;;
-			9)
-				applicationOp ${swarm} composeYml
-				;;
 
-			# Start/stop/restart/rm application
-			10)
-				applicationOp ${swarm} start
-				;;
+			# Application
 			11)
-				applicationOp ${swarm} stop
+				applicationOp ${swarm} logs
 				;;
 			12)
-				applicationOp ${swarm} restart
+				applicationOp ${swarm} start
 				;;
 			13)
+				applicationOp ${swarm} stop
+				;;
+			14)
+				applicationOp ${swarm} restart
+				;;
+			15)
 				applicationOp ${swarm} rm
 				;;
+			16)
+				applicationOp ${swarm} composeYml
+				;;
+			17)
+				applicationOp ${swarm} shell
+				;;
+
+			# Proxy
+			91)
+				followProxyLog ${swarm}
+				;;
+			92)
+				proxyOp ${swarm} Start
+				;;
+			93)
+				proxyOp ${swarm} Stop
+				;;
+			94)
+				proxyOp ${swarm} Restart
+				;;
+			95)
+				proxyOp ${swarm} Remove
+				;;
+			96)
+				showProxyConfig ${swarm}
+				;;
+			97)
+				proxyOp ${swarm} Shell
+				;;
+
 			s)
 				swarmShell ${swarm}
 				;;
@@ -907,6 +914,8 @@ function proxyOp {
 		;;
 	Remove)
 		cmd="rm"
+		;;
+	Shell)
 		;;
 	*)
 		echo "Unknown proxy operation ${op}."
@@ -946,6 +955,19 @@ DOCKER_CERT_PATH=/tmp/docker-certs
 constraint:type==proxy
 EOF
 		echo ""
+
+		# Perhaps do a non-Compose operation
+		if [ "${op}" = "Shell" ] ; then
+			container=$(docker ps | grep "${swarm}-swarm-proxy/stickinthehive_proxy_1" | sed 's!\s.*!!')
+			if [ ! -z ${container} ] ; then
+				trap true SIGINT
+				echo -e "${RED}$ docker exec -it ${container} /bin/bash${PURPLE}"
+				           docker exec -it ${container} /bin/bash
+				trap - SIGINT
+				echo -e -n "${BLUE}"
+			fi
+			return
+		fi
 		
 		# Start/Stop/Restart/Remove the nginx container
 		echo -e ''
@@ -1121,25 +1143,26 @@ function mapAppToSwarm {
 
 	echo ""
 	echo "Available Apps:"
+	echo ""
 	getAppsMappedToSwarm ${swarm}
 	available=( )
 	for app in ${APPS[@]} ; do
-		echo check $app
 		found=N
 		for mapped in ${APPS_FOR_SWARM[@]} ; do
-			echo against ${mapped}
 			[ ${mapped} = ${app} ] && found=Y
 		done
-		echo found is ${found}
 
 		if [ ${found} = 'N' ] ; then
+			echo -e -n ${GREEN}
 			printf "  %s\n" ${app}
 			available+=( ${app} )
 		else
-			printf "  %-20s %s\n" ${app} "(mapped)"
+			echo -e -n ${RED}
+			printf "  %-20s %s\n" ${app} "(already mapped)"
 		fi
 	done
 
+	echo -e ""
 	echo -e -n "${BLUE}Which app would you like to map to swarm ${swarm}? ${RED}"
 	read ans
 
@@ -1162,11 +1185,13 @@ function mapAppToSwarm {
 		return 1
 	fi
 
-echo "App is ${app}"
-	ls -l ${HOME}/apps/${app}
+#echo "App is ${app}"
+#	ls -l ${HOME}/apps/${app}
 
 	getDeploymentModesForApp ${app}
-	if [ ${#MODES_FOR_APP} -eq 0 ] ; then
+
+	#echo MODES is ${DEPLOYMENT_MODES[@]}
+	if [ ${#DEPLOYMENT_MODES[@]} -eq 0 ] ; then
 
 		# Perhaps create a default deployment mode
 		echo -e -n "App ${app} does not define any deployment modes. Create one [y/N]? "
@@ -1209,7 +1234,7 @@ END
 		ready=N
 		echo "Available modes:"
 		while [ ${ready} = N ] ; do
-			for mode in ${MODES_FOR_APP} ; do
+			for mode in ${DEPLOYMENT_MODES} ; do
 				echo "	  ${mode}"
 			done
 			echo -e -n "Mode: "
@@ -1217,8 +1242,8 @@ END
 			[ -z ${ans} ] && return
 
 			cnt=1
-			for m in ${MODES_FOR_APP} ; do
-				if grep -e "${ans}" ${m} ; then
+			for m in ${DEPLOYMENT_MODES} ; do
+				if echo "#{m}" | grep -e "${ans}" ; then
 					mode=${m}
 					cnt=`expr ${cnt} + 1`
 				fi
@@ -1236,10 +1261,12 @@ END
 	# Copy the mode to the swarm
 	modeDir=${HOME}/apps/${app}/deploy/${mode}
 	swarmDir=${HOME}/swarms/${swarm}/${app}
-	echo -e "${RED}$ mkdir -p ${swarmDir}${BLUE}"
-	                 mkdir -p ${swarmDir}
-	echo -e "${RED}$ cp ${modeDir}/* ${swarmDir}${BLUE}"
-	                 cp ${modeDir}/* ${swarmDir}
+	#echo -e "${RED}$ mkdir -p ${swarmDir}${BLUE}"
+	#                 mkdir -p ${swarmDir}
+	#echo -e "${RED}$ cp ${modeDir}/* ${swarmDir}${BLUE}"
+	#                 cp ${modeDir}/* ${swarmDir}
+	echo -e "${RED}ln -s ${modeDir} ${swarmDir}${BLUE}"
+		           ln -s ${modeDir} ${swarmDir}
 
 }
 
@@ -1267,10 +1294,17 @@ function applicationOp {
 		;;
 	rm)
 		label="Remove"
-		cmd="start"
+		cmd="rm"
 		;;
 	composeYml)
 		label="Edit docker-compose.yml for"
+		;;
+	logs)
+		label="View log for"
+		cmd="logs"
+		;;
+	shell)
+		label="Shell in config directory of"
 		;;
 	esac
 	echo1 "${BLUE}"
@@ -1323,6 +1357,16 @@ function applicationOp {
 		vi ${HOME}/swarms/${swarm}/${app}/docker-compose.yml
 		clear
 		return
+	elif [ "${op}" = "shell" ] ; then
+		(
+			cd ${HOME}/swarms/${swarm}/${app}
+			trap true SIGINT
+			echo -e -n ${BLACK}
+			/bin/bash
+			echo -e -n ${BLUE}
+			trap - SIGINT
+		)
+		return
 	fi
 
 	# Perform a docker operation of the application
@@ -1365,11 +1409,12 @@ constraint:type==app
 EOF
 		echo1 ""
 		
-		# Start the nginx container, and limit it to one instance
-		echo -e >&2 "Starting services via Docker Compose"
+		# Perform the docker-compose operation
 		echo -e ''
 		echo -e ${RED}'$ docker-compose '${op}${BLUE}
 		        docker-compose ${op}
+
+		# Limit the number of instances, if starting
 		if [ "${op}" == 'start' ] ; then
 			echo -e ''
 			echo -e ${RED}'$ docker-compose scale app=1'${BLUE}
@@ -1417,11 +1462,11 @@ function getDeploymentModesForApp {
 	DEPLOYMENT_MODES=( )
 	mkdir -p ${HOME}/apps/${app}/deploy
 	for n in ${HOME}/apps/${app}/deploy/* ; do
-		echo ZZ ${n}
+		echo looking at ${n}
 		dir=`basename ${n}`
 		[ "${dir}" != '*' -a "${dir}" != 'docker' ] && DEPLOYMENT_MODES+=( ${dir} )
 	done
-echo modes is ${DEPLOYMENT_MODES[@]}
+	#echo modes is ${DEPLOYMENT_MODES[@]}
 }
 
 # Ask the user to select an application
